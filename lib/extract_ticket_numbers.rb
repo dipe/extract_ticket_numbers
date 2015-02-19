651c62e4995356badc5068937806344ae195a0d1
Peter Ehrenberg <peter.ehrenberg@sinnerschrader.com>
2015-02-19 12:08:50 +0100

    fixed regexp scan

--- lib/extract_ticket_numbers.rb
+++ lib/extract_ticket_numbers.rb
@@ -3,13 +3,18 @@ require 'csv'
 
 module ExtractTicketNumbers
 
-  def self.add_to_csv(in_path, out_path = in_path + '.with_ticket_numbers.csv')
+  def self.add_to_csv(in_path, out_path = out_path_from(in_path))
+    puts out_path
     CSV.open(out_path, "wb", col_sep: ';') do |out_row|
       CSV.foreach(in_path, col_sep: ';', headers: false).each do |in_row|
-        ticket = in_row.to_s.scan[/[a-zA-Z]+\-[0-9]+/].join(', ')
-        in_row << ticket
+        tickets = in_row.to_s.scan(/[a-zA-Z]+\-[0-9]+/)
+        in_row << tickets
         out_row << in_row
       end
     end
+  end
+
+  def self.out_path_from(in_path)
+    File.join(File.dirname(in_path), File.basename(in_path, '.csv') + '-with_ticket_numbers.csv')
   end
 end
