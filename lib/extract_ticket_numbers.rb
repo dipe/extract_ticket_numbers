31e003050b589efc0a1ded99e1e6114fb52bbabd
Peter Ehrenberg <peter.ehrenberg@sinnerschrader.com>
2015-02-19 09:26:00 +0100

    use different extension for out_path

--- lib/extract_ticket_numbers.rb
+++ lib/extract_ticket_numbers.rb
@@ -3,7 +3,7 @@ require 'csv'
 
 module ExtractTicketNumbers
 
-  def self.add_to_csv(in_path, out_path = in_path + '.with_ticket_numbers')
+  def self.add_to_csv(in_path, out_path = in_path + '.with_ticket_numbers.csv')
     CSV.open(out_path, "wb", col_sep: ';') do |out_row|
       CSV.foreach(in_path, col_sep: ';', headers: false).each do |in_row|
         ticket = in_row.to_s.scan[/[a-zA-Z]+\-[0-9]+/].join(', ')
