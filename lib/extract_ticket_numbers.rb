a30277f8a40d889df17d5404e35b701fcd8b5ccd
Peter Ehrenberg <peter.ehrenberg@sinnerschrader.com>
2015-02-20 10:38:33 +0100

    fix bug on mac os x ruby 2.0.0; fix bug with csv format

--- lib/extract_ticket_numbers.rb
+++ lib/extract_ticket_numbers.rb
@@ -5,9 +5,9 @@ module ExtractTicketNumbers
 
   def self.add_to_csv(in_path, out_path = out_path_from(in_path))
     CSV.open(out_path, "wb", col_sep: ';') do |out_row|
-      CSV.foreach(in_path, col_sep: ';', headers: false).each do |in_row|
+      CSV.foreach(in_path, col_sep: ';', headers: false) do |in_row|
         tickets = in_row.to_s.scan(/[a-zA-Z]+\-[0-9]+/)
-        in_row << tickets
+        in_row.concat tickets
         out_row << in_row
       end
     end
